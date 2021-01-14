3154e0a8c591f858de1dc34369e790727ca0a8ef
Patrick Rudolph <patrick.rudolph@9elements.com>
2021-01-14 16:20:33 +0100

    ser2net: Add Gotty support
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- cmd/ser2net/main.go
+++ cmd/ser2net/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -24,12 +25,16 @@ func main() {
 	flag.StringVar(&devPath, "dev", "", "TTY to open")
 	flag.StringVar(&configPath, "config", "", "TTY to open")
 	flag.IntVar(&port, "port", 0, "Telnet port")
+	useTelnet := flag.Bool("telnet", false, "Use telnet")
+	useGotty := flag.Bool("gotty", false, "Use GoTTY")
 
 	flag.Parse()
 	if devPath == "" && configPath == "" {
 		flag.Usage()
 		panic("Error: Device path not set and config not given")
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
 
 	if configPath != "" {
 		var wg sync.WaitGroup
@@ -70,13 +75,48 @@ func main() {
 					baud, _ = strconv.Atoi(opts[0])
 				}
 				fmt.Printf("telnet on port %d baud %d, device %s\n", port, baud, devPath)
-				w, _ := ser2net.NewSerialWorker(devPath, baud)
+				w, _ := ser2net.NewSerialWorker(ctx, devPath, baud)
 				go w.Worker()
 
 				go func() {
 					defer wg.Done()
 
 					err := telnet.ListenAndServe(fmt.Sprintf("%s:%d", bindHostname, port), w)
+					if nil != err {
+						panic(err)
+					}
+				}()
+				wg.Add(1)
+			} else if strings.Contains(scanner.Text(), ":gotty") {
+				conf := strings.Split(scanner.Text(), ":")
+
+				if len(conf) < 4 {
+					continue
+				}
+				if conf[1] != "gotty" {
+					continue
+				}
+				port, _ := strconv.Atoi(conf[0])
+				devPath = conf[3]
+				baud := 115200
+
+				var opts []string
+				if len(conf) > 4 {
+					opts = strings.Split(conf[4], " ")
+				}
+				if len(opts) > 0 {
+					baud, _ = strconv.Atoi(opts[0])
+				}
+				fmt.Printf("gotty on port %d baud %d, device %s\n", port, baud, devPath)
+				w, _ := ser2net.NewSerialWorker(ctx, devPath, baud)
+				go w.Worker()
+
+				go func() {
+					defer wg.Done()
+					if bindHostname == "" {
+						bindHostname = "0.0.0.0"
+					}
+					err := w.StartGoTTY(bindHostname, port, "")
 					if nil != err {
 						panic(err)
 					}
@@ -91,12 +131,26 @@ func main() {
 		wg.Wait()
 
 	} else {
-		w, _ := ser2net.NewSerialWorker(devPath, 0)
+		w, _ := ser2net.NewSerialWorker(ctx, devPath, 0)
 		go w.Worker()
 
-		err := telnet.ListenAndServe(fmt.Sprintf("%s:%d", bindHostname, port), w)
-		if nil != err {
-			panic(err)
+		if useTelnet != nil && *useTelnet {
+			err := w.StartTelnet(bindHostname, port)
+			if nil != err {
+				panic(err)
+			}
+		} else if useGotty != nil && *useGotty {
+			if bindHostname == "" {
+				bindHostname = "0.0.0.0"
+			}
+			err := w.StartGoTTY(bindHostname, port, "")
+			if nil != err {
+				panic(err)
+			}
+		} else {
+			panic("Must specify one of [telnet, gotty]")
 		}
 	}
+
+	cancel()
 }
--- pkg/ser2net/main.go
+++ pkg/ser2net/main.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"github.com/reiver/go-telnet"
+	"github.com/yudai/gotty/server"
+	"github.com/yudai/gotty/utils"
 	"go.bug.st/serial.v1"
 )
 
@@ -29,6 +31,8 @@ type SerialWorker struct {
 	lastErr    string
 	txJobQueue chan byte
 	rxJobQueue []chan byte
+
+	context context.Context
 }
 
 func (w *SerialWorker) connectSerial() {
@@ -48,6 +52,7 @@ func (w *SerialWorker) connectSerial() {
 
 func (w *SerialWorker) txWorker() {
 	for job := range w.txJobQueue {
+
 		if w.connected {
 			_, err := w.serialConn.Write([]byte{job})
 			if err != nil {
@@ -78,7 +83,6 @@ func (w *SerialWorker) rxWorker() {
 	for {
 		b := make([]byte, 1)
 		_, err := w.serialConn.Read(b)
-		fmt.Printf("|%c\n", b)
 
 		if err != nil {
 			if err == syscall.EINTR {
@@ -125,7 +129,7 @@ func (w *SerialWorker) Worker() {
 }
 
 // Serve is invoked by an external entity to provide a Reader and Writer interface
-func (w *SerialWorker) Serve(context context.Context, wr io.Writer, rr io.Reader) {
+func (w *SerialWorker) serve(context context.Context, wr io.Writer, rr io.Reader) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
@@ -210,11 +214,181 @@ func (w *SerialWorker) ServeTELNET(telnetContext telnet.Context, wr telnet.Write
 		return
 	}
 
-	w.Serve(context.Background(), wr, rr)
+	w.serve(context.Background(), wr, rr)
+}
+
+// Close removes the channel from the internal list
+func (w *SerialWorker) Close(rx chan byte) {
+	// Remove RX fifo
+	w.mux.Lock()
+	var new []chan byte
+
+	for i := range w.rxJobQueue {
+		if w.rxJobQueue[i] != rx {
+			new = append(new, w.rxJobQueue[i])
+		}
+	}
+	w.rxJobQueue = new
+	w.mux.Unlock()
+	return
+}
+
+// Open adds a channel to the internal list
+func (w *SerialWorker) Open() (rx chan byte) {
+	rx = make(chan byte, 4096)
+
+	// Add RX fifo
+	w.mux.Lock()
+	w.rxJobQueue = append(w.rxJobQueue, rx)
+	w.mux.Unlock()
+
+	return
+}
+
+// Name returns the instance name
+func (w *SerialWorker) Name() (name string) {
+	name = "go-ser2net"
+	return
+}
+
+// GoTTYWorker used as GoTTY factory
+type GoTTYWorker struct {
+	w          *SerialWorker
+	rx         chan byte
+	lastRxchar byte
+	lastTxchar byte
+}
+
+// Read implements gotty slave interface
+func (g GoTTYWorker) Read(buffer []byte) (n int, err error) {
+
+	b := <-g.rx
+
+	if b == '\n' && g.lastRxchar != '\r' {
+		if n < len(buffer) {
+			buffer[n] = '\r'
+			n++
+		}
+
+	}
+	if n < len(buffer) {
+		buffer[n] = b
+		n++
+	}
+
+	g.lastRxchar = b
+
+	return
+}
+
+// Write implements gotty slave interface
+func (g GoTTYWorker) Write(buffer []byte) (n int, err error) {
+
+	for _, p := range buffer {
+
+		if g.lastTxchar == '\r' && p != '\n' {
+			g.w.txJobQueue <- g.lastTxchar
+			g.w.txJobQueue <- p
+		}
+		g.lastTxchar = p
+		if p == '\r' {
+			g.w.txJobQueue <- '\n'
+			n++
+			continue
+		} else if p == 0x7f {
+			g.w.txJobQueue <- '\b'
+			n++
+			continue
+		}
+		g.w.txJobQueue <- p
+		n++
+	}
+
+	return
+}
+
+// Close implements gotty slave interface
+func (g GoTTYWorker) Close() (err error) {
+	g.w.Close(g.rx)
+	return
+}
+
+// ResizeTerminal implements gotty slave interface
+func (g GoTTYWorker) ResizeTerminal(columns int, rows int) (err error) {
+
+	return
+}
+
+// WindowTitleVariables implements gotty slave interface
+func (g GoTTYWorker) WindowTitleVariables() (titles map[string]interface{}) {
+	titles = map[string]interface{}{
+		"command": "go-ser2net",
+	}
+	return
+}
+
+// New returns a GoTTY slave
+func (w *SerialWorker) New(params map[string][]string) (s server.Slave, err error) {
+	rx := w.Open()
+	s = GoTTYWorker{w: w,
+		rx: rx,
+	}
+
+	return
+}
+
+// StartGoTTY starts a GoTTY server
+func (w *SerialWorker) StartGoTTY(address string, port int, basicauth string) (err error) {
+	htermOptions := &server.HtermPrefernces{}
+	appOptions := &server.Options{
+		Preferences: htermOptions,
+	}
+	if err = utils.ApplyDefaultValues(appOptions); err != nil {
+		return
+	}
+	appOptions.PermitWrite = true
+	appOptions.Address = address
+	appOptions.EnableReconnect = true
+	appOptions.Port = fmt.Sprintf("%d", port)
+	appOptions.EnableBasicAuth = len(basicauth) > 0
+	appOptions.Credential = basicauth
+	appOptions.Preferences.BackspaceSendsBackspace = true
+	hostname, _ := os.Hostname()
+
+	appOptions.TitleVariables = map[string]interface{}{
+		"command":  os.Args[0],
+		"argv":     os.Args[1:],
+		"hostname": hostname,
+	}
+
+	err = appOptions.Validate()
+	if err != nil {
+		return
+	}
+
+	srv, err := server.New(w, appOptions)
+	if err != nil {
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	gCtx, gCancel := context.WithCancel(context.Background())
+
+	srv.Run(ctx, server.WithGracefullContext(gCtx))
+
+	cancel()
+	gCancel()
+	return
+}
+
+// StartTelnet starts a telnet server
+func (w *SerialWorker) StartTelnet(bindHostname string, port int) (err error) {
+	return telnet.ListenAndServe(fmt.Sprintf("%s:%d", bindHostname, port), w)
+
 }
 
 // NewSerialWorker creates a new SerialWorker and connect to path with 115200N8
-func NewSerialWorker(path string, baud int) (*SerialWorker, error) {
+func NewSerialWorker(context context.Context, path string, baud int) (*SerialWorker, error) {
 	var w SerialWorker
 	w.txJobQueue = make(chan byte, 4096)
 	if baud <= 0 {
@@ -227,6 +401,7 @@ func NewSerialWorker(path string, baud int) (*SerialWorker, error) {
 	w.path = path
 	w.connected = false
 	w.lastErr = "Serial is not connected"
+	w.context = context
 
 	return &w, nil
 }
