ea99947762719bc4786bcef728b516b45d495622
Patrik Tesarik <mail@patrik-tesarik.de>
2022-02-01 14:29:59 +0100

    Test rx, renable tx map
    Signed-off-by: Patrik Tesarik <mail@patrik-tesarik.de>

--- go.mod
+++ go.mod
@@ -1,21 +1,12 @@
-module github.com/9elements/go-ser2net
+module github.com/9elements.com/go-ser2net
 
 go 1.14
 
 require (
-	github.com/NYTimes/gziphandler v1.1.1 // indirect
+	github.com/9elements/go-ser2net v0.0.0-20210518094450-dc0adcca6e31
 	github.com/PatrickRudolph/telnet v0.0.0-20210301083732-6a03c1f7971f
-	github.com/codegangsta/cli v1.22.5 // indirect
-	github.com/creack/goselect v0.1.1 // indirect
-	github.com/elazarl/go-bindata-assetfs v1.0.1 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/hashicorp/go-multierror v1.1.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/yudai/gotty v1.0.1
-	github.com/yudai/hcl v0.0.0-20151013225006-5fa2393b3552 // indirect
 	go.bug.st/serial.v1 v0.0.0-20191202182710-24a6610f0541
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 )
 
 replace github.com/yudai/gotty v1.0.1 => github.com/yudai/gotty v2.0.0-alpha.3+incompatible
--- pkg/ser2net/main.go
+++ pkg/ser2net/main.go
@@ -249,7 +249,7 @@ type SerialIOWorker struct {
 	w          *SerialWorker
 	rx         chan byte
 	lastRxchar byte
-	//	lastTxchar byte
+	lastTxchar byte
 }
 
 // Read implements gotty slave interface
@@ -259,13 +259,13 @@ func (g *SerialIOWorker) Read(buffer []byte) (n int, err error) {
 	b = <-g.rx
 
 	for {
-		if b == '\n' && g.lastRxchar != '\r' {
-			if n < len(buffer) {
-				buffer[n] = '\r'
-				n++
-			}
+		//if b == '\n' && g.lastRxchar != '\r' {
+		//	if n < len(buffer) {
+		//		buffer[n] = '\r'
+		//		n++
+		//	}
 
-		}
+		//}
 		if n < len(buffer) {
 			buffer[n] = b
 			n++
@@ -292,22 +292,21 @@ func (g *SerialIOWorker) Write(buffer []byte) (n int, err error) {
 
 	for _, p := range buffer {
 
-		/* Test serial without character mapping
-		/if g.lastTxchar == '\r' && p != '\n' {
-		/	g.w.txJobQueue <- g.lastTxchar
-		/	g.w.txJobQueue <- p
-		/}
-		/g.lastTxchar = p
-		/if p == '\r' {
-		/	g.w.txJobQueue <- '\n'
-		/	n++
-		/	continue
-		/} else if p == 0x7f {
-		/	g.w.txJobQueue <- '\b'
-		/	n++
-		/	continue
-		/	}
-		*/
+		if g.lastTxchar == '\r' && p != '\n' {
+			g.w.txJobQueue <- g.lastTxchar
+			g.w.txJobQueue <- p
+		}
+		g.lastTxchar = p
+		if p == '\r' {
+			g.w.txJobQueue <- '\n'
+			n++
+			continue
+		} else if p == 0x7f {
+			g.w.txJobQueue <- '\b'
+			n++
+			continue
+		}
+
 		g.w.txJobQueue <- p
 		n++
 
